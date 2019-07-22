bfd207f9bebd867bad6c68c28586f76e79b63bbe
Olivier Peyrusse <ope@activeviam.com>
2019-07-22 14:27:26 +0200

    First commit with the app.

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import LeavingScreen from '../screens/LeavingScreen';
+
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {},
+});
+
+const HomeStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  config
+);
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+HomeStack.path = '';
+
+const LinksStack = createStackNavigator(
+  {
+    Links: LinksScreen,
+  },
+  config
+);
+
+LinksStack.navigationOptions = {
+  tabBarLabel: 'Links',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
+  ),
+};
+
+LinksStack.path = '';
+
+const SettingsStack = createStackNavigator(
+  {
+    Settings: SettingsScreen,
+  },
+  config
+);
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
+  ),
+};
+
+SettingsStack.path = '';
+
+const LeavingOfficeStack = createStackNavigator(
+  {
+    Leaving: LeavingScreen
+  },
+  config);
+
+LeavingOfficeStack.navigationOptions = {
+  tabBarLabel: 'Leaving',
+  tabBarIcon: ({focused}) => (
+    <TabBarIcon focused={focused} name={Platform.IO === 'ios' ? 'ios-link' : 'md-link'} />
+  )
+};
+
+const tabNavigator = createBottomTabNavigator({
+  HomeStack,
+  LinksStack,
+  SettingsStack,
+  LeavingOfficeStack,
+});
+
+tabNavigator.path = '';
+
+export default tabNavigator;
---
+++ screens/LeavingScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ScrollView, StyleSheet, Text, Button } from 'react-native';
+
+export default function LeavingScreen() {
+  const notifyHome = () => {
+    alert("Notification");
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      <Text>Olivier, it's time to leave</Text>
+      <Button
+        onPress={notifyHome}
+        title="Notify Home <3"
+        color="#841584"
+        accessibilityLabel="Send a SMS notification to Colombe"
+      />
+    </ScrollView>
+  );
+}
+
+LeavingScreen.navigationOptions = {
+  title: 'Leaving the office',
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 15,
+  },
+});
