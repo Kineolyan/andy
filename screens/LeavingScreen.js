07fca271edd8d6a2121f1d748041b8d55ee66817
Kineolyan <kineolyan@gmail.com>
2019-08-26 21:54:43 +0200

    Add methods to launch the SMS.

--- screens/LeavingScreen.js
+++ screens/LeavingScreen.js
@@ -1,25 +1,89 @@
-import React from 'react';
-import { ScrollView, StyleSheet, Text, Button } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { ScrollView, StyleSheet, Text, Button, Alert } from 'react-native';
 import * as sms from 'expo-sms';
 
-const availability = sms.isAvailableAsync();
+async function notifyCo(message) {
+  const { result } = await sms.sendSMSAsync(
+    '+33663053180',
+    message
+  );
+  return result;
+}
+
+function notifyHome() {
+  return notifyCo("Je pars du bureau. À tout à l'heure");
+}
+
+function notifyProgress() {
+  return notifyCo('Je suis à la gare. Prochain train dans ');
+}
+
+function confirmSms(message) {
+  return Alert.alert(
+    'SMS result',
+    message);
+}
+
+async function sendSms(button, notifyFn, cbk) {
+  cbk(button);
+  const result = notifyFn();
+  cbk(null);
+  switch (result) {
+  case 'cancelled':
+    return confirmSms('SMS cancelled');
+  case 'sent':
+    return confirmSms('SMS envoyé');
+  case 'unknown':
+  default:
+    return confirmSms('Error when sending SMS');
+  }
+}
+
+const OFFICE_BUTTON = 'office';
+const STATION_BUTTON = 'station';
 
 export default function LeavingScreen() {
-  const notifyHome = () => {
-    availability.then(a => alert(`SMS availability: ${a}`));
-  };
+  const [availability, setAvailability] = useState(null);
+  const [sendingSms, setSmsInProgress] = useState(null);
+  useEffect(() => {
+    sms.isAvailableAsync().then(a => setAvailability(a));
+  });
 
-  return (
-    <ScrollView style={styles.container}>
-      <Text>Olivier, it's time to leave</Text>
-      <Button
-        onPress={notifyHome}
-        title="Notify Home <3"
-        color="#841584"
-        accessibilityLabel="Send a SMS notification to Colombe"
-      />
-    </ScrollView>
-  );
+  if (availability === null) {
+    return (
+      <ScrollView style={styles.container}>
+        <Text>Test availability of the SMS service...</Text>
+      </ScrollView>);
+  } else {
+    const buttonStyles = {
+      [OFFICE_BUTTON]: {
+        disabled: false
+      },
+      [STATION_BUTTON]: {
+        disabled: false
+      }
+    };
+    if (sendingSms !== null) {
+      buttonStyles[sendingSms].disabled = true;
+    }
+
+    return (
+      <ScrollView style={styles.container}>
+        <Text>Olivier, it's time to leave</Text>
+        <Button
+          onPress={() => sendSms(OFFICE_BUTTON, notifyHome, setSmsInProgress)}
+          title="Notify Home <3"
+          color="#841584"
+          accessibilityLabel="Send a SMS notification from the office"
+        />
+        <Button
+          onPress={() => sendSms(STATION_BUTTON, notifyProgress, setSmsInProgress)}
+          title="Notify progress"
+          color="#0c0c0c"
+          accessibilityLabel="Send a SMS notification from the station"
+        />
+      </ScrollView>);
+  }
 }
 
 LeavingScreen.navigationOptions = {
